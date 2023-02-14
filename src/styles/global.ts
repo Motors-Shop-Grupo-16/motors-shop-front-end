7e028b687b3389afbbeb4f253c1ce8b4329059cf
Leandro Schillreff <leandroschillreff@gmail.com>
2023-02-14 11:54:26 -0400

    feat: added global style

---
+++ src/styles/global.ts
@@ -0,0 +1,46 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyle = createGlobalStyle`
+  :root {
+    --color-brand1: #4529E6;
+    --color-brand2: #5126EA;
+    --color-brand3: #B0A6F0;
+    --color-brand4: #EDEAFD;
+
+    --color-grey0: #0B0D0D;
+    --color-grey1: #212529;
+    --color-grey2: #495057;
+    --color-grey3: #868E96;
+    --color-grey4: #ADB5BD;
+    --color-grey5: #CED4DA;
+    --color-grey6: #DEE2E6;
+    --color-grey7: #E9ECEF;
+    --color-grey8: #F1F3F5; 
+    --color-grey9: #F8F9FA;
+    --color-grey10:#FDFDFD;
+
+    --color-whiteFixed: #FFFFFF;
+
+    --color-alert1: #CD2B31;
+    --color-alert2: #FDD8D8;
+    --color-alert3: #FFE5E5;
+
+    --color-sucess1: #18794E;
+    --color-sucess2: #CCEBD7;
+    --color-sucess3: #DDF3E4;
+
+    --color-random1: #E34D8C;
+    --color-random2: #C04277;
+    --color-random3: #7D2A4D;
+    --color-random4: #7000FF;
+    --color-random5: #6200E3;
+    --color-random6: #36007D;
+    --color-random7: #349974;
+    --color-random8: #2A7D5F;
+    --color-random9: #153D2E;
+    --color-random10: #6100FF;
+    --color-random11: #5700E3;
+    --color-random12: #30007D;
+
+  } 
+`;
