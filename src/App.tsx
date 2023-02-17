ebc1435ee596a0fcff7094617478a0c64872afcc
Leandro Schillreff <leandroschillreff@gmail.com>
2023-02-17 17:12:18 -0400

    feat: added auction list

--- src/App.tsx
+++ src/App.tsx
@@ -1,3 +1,5 @@
+import { AuctionList } from "./components/Auction/AuctionList";
+import { Container } from "./components/Container";
 import { GlobalStyle } from "./styles/global";
 import { ResetStyle } from "./styles/reset";
 
@@ -6,6 +8,9 @@ function App() {
     <>
       <GlobalStyle />
       <ResetStyle />
+      <Container>
+        <AuctionList />
+      </Container>
     </>
   );
 }
