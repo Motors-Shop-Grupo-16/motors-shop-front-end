9dc6a6531d3d05bac1b81da9831857fac185c142
Vitor Brasil <vitorlbrasil@hotmail.com>
2023-02-22 10:42:42 -0300

    feat: add form components

---
+++ src/components/Form/FormContainer.ts
@@ -0,0 +1,10 @@
+import styled from "styled-components";
+
+export const FormContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  padding: 44px 48px;
+  gap: 24px;
+`;
