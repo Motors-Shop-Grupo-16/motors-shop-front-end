69d98b54d1c9de4e46538350d962d3396e80874c
Vitor Brasil <vitorlbrasil@hotmail.com>
2023-02-23 12:20:59 -0300

    feat: Input and Textarea components

--- src/components/Form/Label.ts
+++
@@ -1,8 +0,0 @@
-import styled from "styled-components";
-
-export const Label = styled.label`
-  font-family: "Inter", sans-serif;
-  font-weight: 500;
-  font-size: 14px;
-  color: var(--color-grey1);
-`;
