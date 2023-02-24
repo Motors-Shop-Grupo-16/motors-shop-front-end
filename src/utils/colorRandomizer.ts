fa6d17b1b4b0fc32d119ae2f2a08babafdc54bcb
Leandro Schillreff <leandroschillreff@gmail.com>
2023-02-24 11:16:17 -0400

    refactor: colorRandomizer util

--- src/utils/colorRandomizer.ts
+++ src/utils/colorRandomizer.ts
@@ -1,5 +1,5 @@
 const colorRandomizer = (name: string) => {
-  return String(Math.round(name.length % 13));
+  return String(Math.round((name.length % 12) + 1));
 };
 
 export default colorRandomizer;
