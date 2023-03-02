fa18d32efd2b8b661839c93bdc6f656980dbd897
Leandro Schillreff <leandroschillreff@gmail.com>
2023-03-02 18:48:36 -0400

    feat: navbar user

--- src/masks/index.ts
+++ src/masks/index.ts
@@ -27,5 +27,6 @@ export const dateMask = (value: string) => {
   return value
     .replace(/\D/g, "")
     .replace(/(\d{4})(\d)/, "$1-$2")
-    .replace(/(\d{2})(\d{2})\d+$/, "$1-$2");
+    .replace(/(-\d{2})(\d)/, "$1-$2")
+    .replace(/(-\d{2})\d+$/, "$1");
 };
