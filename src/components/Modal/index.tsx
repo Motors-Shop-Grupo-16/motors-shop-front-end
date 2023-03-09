f1606daee00885463a7af9437d5ce5f3fe46a787
ssidartaa <odssy351@gmail.com>
2023-03-08 23:12:49 -0500

    fix: modal close on click Background

--- src/components/Modal/index.tsx
+++ src/components/Modal/index.tsx
@@ -6,7 +6,7 @@ import { Heading } from "../../styles/typography";
 const Modal = ({ children, title, closeModal }: IModalProps) => {
   return (
     <Container>
-      <div className="containerModal">
+      <div className="containerModal" onClick={() => closeModal(false)}>
         <div className="modal">
           <div className="modalHeader">
             <Heading tag="h3" style="heading-7">
